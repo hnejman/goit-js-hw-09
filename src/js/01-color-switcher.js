14cea537852aa7bc1b5c07641b12af136b123c03
hnejman <108231655+hnejman@users.noreply.github.com>
2023-01-07 13:36:52 +0100

    ...

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
+const body = document.querySelector("body");
+stopBtn.disabled = true;
+body.style.backgroundColor = "#ffffff";
 
+let intervalId;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function setColor(element) {
+    element.style.backgroundColor = getRandomHexColor();
+}
+
+startBtn.addEventListener("click", e => {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    intervalId = setInterval(setColor, 1000, body);
+});
+
+stopBtn.addEventListener("click", e => {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    clearInterval(intervalId);
+});
